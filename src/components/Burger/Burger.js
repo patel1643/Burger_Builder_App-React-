28fc1c572726d53fb790e845cb5294b7c758f1ee
patel1643 <42159154+patel1643@users.noreply.github.com>
2020-06-03 01:03:54 -0400

    dynamic ingredients feature added

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,17 @@ import classes from './Burger.module.css';
 
 
 const burger = ( props ) =>{
+    
+    const dynamicIngredients = Object.keys(props.ingredients).map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+               return <BurgerIngredient key={igKey + i} type={igKey} />
+            });
+        });
+
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="salad"/>
-            <BurgerIngredient type="bacon"/>
-            <BurgerIngredient type="meat"/>
+            {dynamicIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
         
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,20 @@ import Auxillary from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients:{
+            cheese: 2,
+            salad: 2,
+            bacon: 1,
+            meat: 1                        
+        }
+    }
+
     render() {
         return(
             <Auxillary>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls..</div>
             </Auxillary>
         );
