b01e027731da1ee734d791ee044a58efaf6cb5de
patel1643 <42159154+patel1643@users.noreply.github.com>
2020-06-14 02:10:25 -0400

    Updates

---
+++ precache-manifest.adf325ebdb0f48d2e8ce457bf52f4e50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812fbf2ed83ca31e96e232a304a3e0cc",
+    "url": "/Burger_Builder_App-React-/index.html"
+  },
+  {
+    "revision": "8c546279a3016e90ac3d",
+    "url": "/Burger_Builder_App-React-/static/css/main.8823d4e1.chunk.css"
+  },
+  {
+    "revision": "30f0abc5a9bf1db94cb6",
+    "url": "/Burger_Builder_App-React-/static/js/2.ea53f59d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger_Builder_App-React-/static/js/2.ea53f59d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c546279a3016e90ac3d",
+    "url": "/Burger_Builder_App-React-/static/js/main.330bbf8a.chunk.js"
+  },
+  {
+    "revision": "8f43e88d7d1d42d9af6c",
+    "url": "/Burger_Builder_App-React-/static/js/runtime-main.4aa11c64.js"
+  }
+]);
