1dd08fa2ec145d0708f12dfd11efc1054d001a97
patel1643 <42159154+patel1643@users.noreply.github.com>
2021-01-07 04:19:01 -0500

    added back end partially

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Layout from './components/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+
+import Checkout from './containers/Checkout/Checkout';
 function App() {
-  // alert('This app is still under development so some functions might not work and I apologize in advance for that, cheers!');
+
   return(
     <div>
       <Layout>
       <BurgerBuilder />
+      <Checkout />
       </Layout>
       
     </div>
